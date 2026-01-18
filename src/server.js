595c0902ccca02b3bee6be43edf72243e9ba7554
Itshaseeb791 <muh203203@gmail.com>
2026-01-18 20:20:58 +0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+// Bind to 0.0.0.0 to accept external connections
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
