a4b52aea4f8dfc4da89863fca16dccb808df8c86
Itshaseeb791 <muh203203@gmail.com>
2026-02-25 18:54:12 +0500

    update the port to 4000

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = 5000;
+const PORT = 4000;
 
 // Bind to 0.0.0.0 to accept external connections
 app.listen(PORT, "0.0.0.0", () => {
