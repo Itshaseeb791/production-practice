a04624b23262e9cadee7625de0a3c9eb9710aebd
Itshaseeb791 <muh203203@gmail.com>
2026-02-25 17:24:13 +0500

    update code to version v1 which is manual deployment

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.send("API running on Vercel 🚀");
+  res.send("API running on production v1");
 });
 
 module.exports = app;
