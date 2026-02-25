a8be374c43498bb4600e4b2137010cdcc20f0806
Itshaseeb791 <muh203203@gmail.com>
2026-02-26 01:06:43 +0500

    update the server js according to pm2

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const PORT = 4000;
 
 // Bind to 0.0.0.0 to accept external connections
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} with version v3` );
 });
